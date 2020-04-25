d730c119ee0e777fc67d88422bc22cde24588020
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-25 15:42:40 +0200

    sleep modified

--- functions/email_sender.js
+++ functions/email_sender.js
@@ -2,14 +2,19 @@ const {transporter}=require('../startup/email_sender')
 const {Threshold}=require('../models/ag_threshold')
 const {User}=require('../models/user')
 
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 async function email_sender(array){
     const res = await User.find({})
 
     var i=0;
     for(i=0;i<res.length;i++)
     {
-        console.log(res[i].email)
-        let info=transporter.sendMail({
+        await sleep(2000).then(() => {
+            console.log(res[i].email)
+            let info=transporter.sendMail({
             from: '"Progetto Diana" <progetto-diana@libero.it>', 
             to: res[i].email, 
             subject: "Attenzione", 
@@ -17,14 +22,8 @@ async function email_sender(array){
             html: "<body><h1> Livello critico raggiunto:</h1><br> Raggiunto livello di "+ 
                     array.type+" pari a "+array.value+" in data "+array.date+" presso il sensore "
                     +array.sensore+ "("+array.lat+", "+array.lon+")</body>", 
+            })
         })
-
-        function sleep(s){
-            var now = new Date().getTime();
-            while(new Date().getTime() < now + (s*1000)){ /* non faccio niente */ } 
-        }
-
-        sleep(1)
         
     }
 
