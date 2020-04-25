1d071741f21aacd2ce799e955061f6396e444a9a
Ivan Giacomoni <giacomoni.1796069@studenti.uniroma1.it>
2020-04-25 17:33:28 +0200

    udated with sleep

--- amqp/consumer.js
+++ amqp/consumer.js
@@ -4,6 +4,10 @@ const config = require('config')
 const {email_sender} = require('../functions/email_sender')
 //const {frontend_comunicate} = require('../websockets/ws')
 
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 function amqpStartUp() {
     amqp.connect(config.get('amqp_server'), function(error0, connection) {
         if (error0) {
@@ -20,7 +24,11 @@ function amqpStartUp() {
                 durable: false
             })
 
-            channel.consume(queue, function(msg) {
+            channel.consume(queue, async function(msg) {
+                if(msg == undefined){
+                    console.log('Amqp server started late')
+                    return
+                }
                 // Taking chemical agents from Diana that must be analyzed
                 const arr_chemical_agents = JSON.parse(msg.content)
                 const dim = arr_chemical_agents.length
@@ -29,118 +37,119 @@ function amqpStartUp() {
                 let soglia_superata = false
 
                 for(i=0;i<dim;i++){
-
-                    // Taking info about the chemical_agent in position i
-                    const reg_date = arr_chemical_agents[i].reg_date
-                    const value = arr_chemical_agents[i].value
-                    const type = arr_chemical_agents[i].types
-                    const sensor = arr_chemical_agents[i].sensor
-                    const lat = arr_chemical_agents[i].lat
-                    const lon = arr_chemical_agents[i].long
+                    await sleep(2000).then(() => {
+                        // Taking info about the chemical_agent in position i
+                        const reg_date = arr_chemical_agents[i].reg_date
+                        const value = arr_chemical_agents[i].value
+                        const type = arr_chemical_agents[i].types
+                        const sensor = arr_chemical_agents[i].sensor
+                        const lat = arr_chemical_agents[i].lat
+                        const lon = arr_chemical_agents[i].long
                 
-                    // Checking tresholds
-                    if(value >= 101 && value <= 150){
-                        soglia_superata = true
+                        // Checking tresholds
+                        if(value >= 101 && value <= 150){
+                            soglia_superata = true
 
-                        const toSaveInDb = {
-                            type: type,
-                            sensore: sensor,
-                            value: value,
-                            date: reg_date,
-                            lon: lon,
-                            lat: lat
-                        }
-                        // Sending an email and saving into the database
-                        email_sender(toSaveInDb)
-                        save_to_db(toSaveInDb)
+                            const toSaveInDb = {
+                                type: type,
+                                sensore: sensor,
+                                value: value,
+                                date: reg_date,
+                                lon: lon,
+                                lat: lat
+                            }
+                            // Sending an email and saving into the database
+                            email_sender(toSaveInDb)
+                            save_to_db(toSaveInDb)
 
-                        const forFrontEnd = {
-                            type: type,
-                            value: value,
-                            air_pollution_level: 'Unhealthy for sensitive groups',
-                            color: 'orange'                        
+                            const forFrontEnd = {
+                                type: type,
+                                value: value,
+                                air_pollution_level: 'Unhealthy for sensitive groups',
+                                color: 'orange'                        
+                            }
+                            // Communicating with frontend
+                            // frontend_comunicate(JSON.stringify(forFrontEnd))
                         }
-                        // Communicating with frontend
-                        // frontend_comunicate(JSON.stringify(forFrontEnd))
-                    }
                     
-                    // Checking tresholds
-                    else if(value >= 151 && value <= 200){
-                        soglia_superata = true
+                        // Checking tresholds
+                        else if(value >= 151 && value <= 200){
+                            soglia_superata = true
 
-                        const toSaveInDb = {
-                            type: type,
-                            sensore: sensor,
-                            value: value,
-                            date: reg_date,
-                            lon: lon,
-                            lat: lat
-                        }
-                        // Sending an email and saving into the database
-                        email_sender(toSaveInDb)
-                        save_to_db(toSaveInDb)
+                            const toSaveInDb = {
+                                type: type,
+                                sensore: sensor,
+                                value: value,
+                                date: reg_date,
+                                lon: lon,
+                                lat: lat
+                            }
+                            // Sending an email and saving into the database
+                            email_sender(toSaveInDb)
+                            save_to_db(toSaveInDb)
 
-                        const forFrontEnd = {
-                            type: type,
-                            value: value,
-                            air_pollution_level: 'Unhealthy',
-                            color: 'red'                        
+                            const forFrontEnd = {
+                                type: type,
+                                value: value,
+                                air_pollution_level: 'Unhealthy',
+                                color: 'red'                        
+                            }
+                            // Communicating with frontend
+                            // frontend_comunicate(JSON.stringify(forFrontEnd))
                         }
-                        // Communicating with frontend
-                        // frontend_comunicate(JSON.stringify(forFrontEnd))
-                    }
                     
-                    // Checking tresholds
-                    else if(value >= 201 && value <= 300){
-                        soglia_superata = true
+                        // Checking tresholds
+                        else if(value >= 201 && value <= 300){
+                            soglia_superata = true
 
-                        const toSaveInDb = {
-                            type: type,
-                            sensore: sensor,
-                            value: value,
-                            date: reg_date,
-                            lon: lon,
-                            lat: lat
-                        }
-                        // Sending an email and saving into the database
-                        email_sender(toSaveInDb)
-                        save_to_db(toSaveInDb)
+                            const toSaveInDb = {
+                                type: type,
+                                sensore: sensor,
+                                value: value,
+                                date: reg_date,
+                                lon: lon,
+                                lat: lat
+                            }
+                            // Sending an email and saving into the database
+                            email_sender(toSaveInDb)
+                            save_to_db(toSaveInDb)
                         
-                        const forFrontEnd = {
-                            type: type,
-                            value: value,
-                            air_pollution_level: 'Very Unhealthy',
-                            color: 'violet'                        
+                            const forFrontEnd = {
+                                type: type,
+                                value: value,
+                                air_pollution_level: 'Very Unhealthy',
+                                color: 'violet'                        
+                            }
+                            // Communicating with frontend
+                            // frontend_comunicate(JSON.stringify(forFrontEnd))
                         }
-                        // Communicating with frontend
-                        // frontend_comunicate(JSON.stringify(forFrontEnd))
-                    }
-                    
-                    // Checking tresholds
-                    else if(value >= 301){
-                        soglia_superata = true
 
-                        const toSaveInDb = {
-                            type: type,
-                            sensore: sensor,
-                            value: value,
-                            date: reg_date,
-                            lon: lon,
-                            lat: lat
-                        }
-                        // Sending an email and saving into the database
-                        email_sender(toSaveInDb)
-                        save_to_db(toSaveInDb)
+                        // Checking tresholds
+                        else if(value >= 301){
+                            soglia_superata = true
 
-                        const forFrontEnd = {
-                            type: type,
-                            value: value,
-                            air_pollution_level: 'Hazardous',
-                            color: 'brown'                        
+                            const toSaveInDb = {
+                                type: type,
+                                sensore: sensor,
+                                value: value,
+                                date: reg_date,
+                                lon: lon,
+                                lat: lat
+                            }
+                            // Sending an email and saving into the database
+                            email_sender(toSaveInDb)
+                            save_to_db(toSaveInDb)
+
+                            const forFrontEnd = {
+                                type: type,
+                                value: value,
+                                air_pollution_level: 'Hazardous',
+                                color: 'brown'                        
+                            }
+                            // Communicating with frontend
+                            // frontend_comunicate(JSON.stringify(forFrontEnd))
                         }
-                        // Communicating with frontend
-                        // frontend_comunicate(JSON.stringify(forFrontEnd))
-                    }
+                    })
                 }
 
                 if(!soglia_superata){
